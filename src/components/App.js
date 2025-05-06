97635ac3fc3d09bcabcfaff6ff078c5aff5b9a12
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-05-06 18:11:50 +0530

    updated version

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
+import React, { useEffect, useMemo, useState } from 'react'
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  let [users,setUsers]=useState([])
+  const [loading, setLoading] = useState(true);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response=>response.json())
+  .then(data=>{
+    setTimeout(()=>{
+      setUsers(data)
+      setLoading(false)
+    },300)
+  })
+  .catch(err=>console.log('error occured',err))
+  },[])
+  
+  const memorizedUser=useMemo(()=>users,[users])
 
-const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {
+        loading ?(
+          <div>Loading...</div>
+        ):(
+          <ul>
+      { 
+        memorizedUser.map((user)=>(
+          <div key={user.id}  style={{marginBottom:'20px'}} >
+            <li><strong>{user.title}</strong></li>
+            <dl>{user.body}</dl>
+          </div>
+        ))
+      }
+      </ul>
+        )
+      }
     </div>
   )
 }
